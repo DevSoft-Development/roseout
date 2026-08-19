6668668f561c640980348cd66eb2e634ac6b7d87
roseoutllc <nick@roseout.com>
2026-08-18 21:31:53 -0400

    Stop injecting communication center on CRM home

--- components/admin/crm/CrmWorkspaceShell.tsx
+++ components/admin/crm/CrmWorkspaceShell.tsx
@@ -1,20 +1,7 @@
-"use client";
-
-import { usePathname } from "next/navigation";
-import CommunicationCenter from "@/components/admin/crm/CommunicationCenter";
-
 export default function CrmWorkspaceShell({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const isCrmHome = pathname === "/admin/dashboard/crm";
-
-  return (
-    <section className="min-w-0 space-y-5">
-      {isCrmHome ? <CommunicationCenter /> : null}
-      {children}
-    </section>
-  );
+  return <section className="min-w-0 space-y-5">{children}</section>;
 }
