41870f65bb9a21d157ce7f33057e719a9e16acac
roseoutllc <nick@roseout.com>
2026-08-18 23:29:57 -0400

    Add CRM Location Health page

---
+++ app/admin/dashboard/crm/location-health/page.tsx
@@ -0,0 +1,15 @@
+import CrmWorkspaceShell from "@/components/admin/crm/CrmWorkspaceShell";
+import { requireAdminRole } from "@/lib/admin-auth";
+import { ADMIN_PAGE_ACCESS } from "@/lib/admin-permissions";
+import LocationHealthClient from "./LocationHealthClient";
+
+export const dynamic = "force-dynamic";
+
+export default async function LocationHealthPage() {
+  await requireAdminRole(ADMIN_PAGE_ACCESS.crm);
+  return (
+    <CrmWorkspaceShell>
+      <LocationHealthClient />
+    </CrmWorkspaceShell>
+  );
+}
