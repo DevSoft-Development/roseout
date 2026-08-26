01fe6e6ce9ce4686ca639d2e70af90848e1d7e42
roseoutllc <nick@roseout.com>
2026-08-25 23:40:18 -0400

    Avoid routing generic Book Plan booking replies to Reservations

--- lib/concierge/department-routing.ts
+++ lib/concierge/department-routing.ts
@@ -1,6 +1,6 @@
 export type ConciergeDepartment = "concierge" | "reservations" | "support";
 
-const RESERVATION_INTENT = /\b(my\s+)?(reservation|booking|table|waitlist)\b|\b(cancel|change|move|reschedule|modify)\b.{0,40}\b(reservation|booking|table|time|date|party\s*size)\b|\b(reservation|booking)\b.{0,40}\b(cancel|change|move|reschedule|modify|confirmation|details)\b/i;
+const RESERVATION_INTENT = /\b(reservation|waitlist)\b|\b(cancel|change|move|reschedule|modify)\b.{0,40}\b(reservation|table|party\s*size)\b|\bmy\s+booking\b.{0,40}\b(cancel|change|move|reschedule|modify|confirmation|details)\b/i;
 
 const SUPPORT_INTENT = /\b(password|passcode|log\s*in|login|sign\s*in|signin|account\s+access|reset\s+(?:my\s+)?password|forgot\s+(?:my\s+)?password|verification\s+code|authentication\s+code|locked\s+out|billing|subscription|invoice|refund|chargeback|charged|technical\s+(?:issue|problem)|support\s+(?:agent|team)|customer\s+support)\b/i;
 
