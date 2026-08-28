b5fdfb172bfeca023ce95096457096e2c38950c9
roseoutllc <nick@roseout.com>
2026-08-28 17:32:41 -0400

    Fix search wrapper exports for typecheck

--- lib/search/runSearch.ts
+++ lib/search/runSearch.ts
@@ -2,7 +2,6 @@ import type { EnterpriseSearchResult } from "@/lib/search/enterprise/types";
 import { runOutingSearch as runOutingSearchImpl, type RunOutingSearchInput } from "./runSearchImpl";
 import { withSearchUserContext } from "./searchUserContext";
 
-export * from "./runSearchImpl";
 export type { RunOutingSearchInput };
 
 export async function runOutingSearch(input: RunOutingSearchInput): Promise<EnterpriseSearchResult> {
