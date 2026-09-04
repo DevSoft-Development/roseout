f5e781d6aa64925e87cf4df6f6dc7f911f942bd4
roseoutllc <nick@roseout.com>
2026-09-04 17:02:15 -0400

    chore: rebase Search V2 orphan cleanup on latest main (#2515)

--- lib/search/v2/response/buildDebugSearchResponse.ts
+++
@@ -1,2 +0,0 @@
-import type { PublicSearchResponseV2 } from "./responseTypes"; import type { SearchTrace } from "../observability/searchTrace";
-export function buildDebugSearchResponse(response: PublicSearchResponseV2, trace: SearchTrace) { return { ...response, debug: { trace } }; }
