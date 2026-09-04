9b27ab61dd767f1c32eb9e31982cd58566525292
roseoutllc <nick@roseout.com>
2026-09-04 13:16:48 -0400

    chore: rebase email orphan cleanup on latest main

--- lib/email/emailDepartments.ts
+++
@@ -1,7 +0,0 @@
-export type { EmailDepartment } from "./types";
-export { DEPARTMENT_LABELS as EMAIL_DEPARTMENTS, normalizeEmailDepartment as getEmailDepartment } from "./brand";
-import { resolveEmailSender } from "./brand";
-
-export function getEmailSignature(department?: string | null): string {
-  return resolveEmailSender(department).signature;
-}
