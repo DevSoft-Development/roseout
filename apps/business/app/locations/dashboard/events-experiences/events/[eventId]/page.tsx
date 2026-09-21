a73fc91cca95fa33a7fe40f41cfa840dbc619bd5
roseoutllc <nick@roseout.com>
2026-09-21 11:39:04 -0400

    Remove obsolete detail page import

--- apps/business/app/locations/dashboard/events-experiences/events/[eventId]/page.tsx
+++ apps/business/app/locations/dashboard/events-experiences/events/[eventId]/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { redirect } from "next/navigation";
 import EventOrderRefundButton from "@/components/events/EventOrderRefundButton";
 import { createClient } from "@/lib/supabase-server";
