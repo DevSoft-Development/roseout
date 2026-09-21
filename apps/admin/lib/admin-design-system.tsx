8c0583008e50b0100f33ee4eb067a65e5c63d7f6
roseoutllc <nick@roseout.com>
2026-09-21 10:44:45 -0400

    Align Admin overview and remaining consistency surfaces (#3233)
    * Align Admin consistency surfaces from current main
    
    * Align Admin consistency surfaces from current main
    
    * Align Admin consistency surfaces from current main
    
    * Align Admin consistency surfaces from current main
    
    * Align Admin consistency surfaces from current main
    
    * Add isolated Admin design system re-export

---
+++ apps/admin/lib/admin-design-system.tsx
@@ -0,0 +1,14 @@
+export {
+  AdminActionButton,
+  AdminEmptyState,
+  AdminKpiCard,
+  AdminKpiGrid,
+  AdminPageHeader,
+  AdminPageShell,
+  AdminSectionCard,
+  AdminStatusBadge,
+  formatAdminDate,
+  formatAdminNumber,
+  getReadinessLabel,
+  getReadinessTone,
+} from "../components/admin/AdminDesignSystem";
