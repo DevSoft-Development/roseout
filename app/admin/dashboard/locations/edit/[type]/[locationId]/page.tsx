40fb8fc9b26f94f01964024a8f150e8cbcce4b95
roseoutllc <nick@roseout.com>
2026-09-23 19:16:35 -0400

    Route legacy location workspace into CRM

--- app/admin/dashboard/locations/edit/[type]/[locationId]/page.tsx
+++ app/admin/dashboard/locations/edit/[type]/[locationId]/page.tsx
@@ -1,1 +1,10 @@
-export { default } from "../../../../../../locations/[type]/[locationId]/edit/page";
+import { redirect } from "next/navigation";
+
+export default async function LegacyLocationEdit({
+  params,
+}: {
+  params: Promise<{ type: string; locationId: string }>;
+}) {
+  const { locationId } = await params;
+  redirect(`/admin/dashboard/crm/${locationId}?tab=profile`);
+}
